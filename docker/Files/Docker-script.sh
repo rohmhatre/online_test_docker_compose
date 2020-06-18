288a657b84ea62c2bad833ae1a4eb7487fb987b8
Rohan Mhatre <roh.mhatre@gmail.com>
2020-06-18 19:05:07 +0530

    Added celery and redis service

--- docker/Files/Docker-script.sh
+++ docker/Files/Docker-script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash 
-chown -R www-data /Sites/online_test
-chown -R www-data /Sites/online_test/yaksh
+chown -R apache /Sites/online_test
+chown -R apache /Sites/online_test/yaksh
 chown -R nobody /Sites/online_test/yaksh_data
 chmod -R 664 /Sites/online_test
 chmod -R +X /Sites
-/usr/sbin/apache2ctl -D FOREGROUND
+cd /Sites/online_test
+/etc/init.d/celeryd start
+python3 manage.py makemigrations
+python3 manage.py makemigrations notifications_plugin
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'\*'\]/g" /Sites/online_test/online_test/settings.py
+sed -i "s/'redis\:\/\/localhost'/'redis\:\/\/yaksh\-redis'/g" /Sites/online_test/online_test/settings.py
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python3 manage.py shell
+/usr/sbin/httpd -D FOREGROUND
